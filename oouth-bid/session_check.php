5a51a8273c2a91c263b7da45db8efc643c0c67d0
Bankole Abiodun <110631073+meowbanky@users.noreply.github.com>
2024-09-23 12:24:50 +0100

    Untrack files listed in .gitignore

--- oouth-bid/session_check.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-    if ((!isset($_SESSION['company_id']))||(!isset($_SESSION['SESS_MEMBER_ID']))){
-                header("location: index.php");
-		exit();
-        
-    }
-
-?>
