54faf0ef14efba45686aff576b25b923a8c17bc3
Bankole Abiodun <110631073+meowbanky@users.noreply.github.com>
2024-09-26 22:30:19 +0100

    updated setpassword

--- setpassword.php
+++ setpassword.php
@@ -31,8 +31,8 @@ $tokenCheck = $database->passwordReset($token);
         <div class="h-screen w-screen flex justify-center items-center">
 
             <div class="2xl:w-1/4 lg:w-1/3 md:w-1/2 w-full">
-                <div class="card overflow-hidden sm:rounded-md rounded-none">
-                    <div class="p-6">
+                <div class="card overflow-hidden sm:rounded-md rounded-none bg-gray-50 dark:bg-gray-800 shadow-md">
+                <div class="p-6">
                         <a href="index.php" class="block mb-8">
                             <img class="h-6 block dark:hidden" src="assets/images/logo-dark.png" alt="">
                             <img class="h-6 hidden dark:block" src="assets/images/logo-light.png" alt="">
